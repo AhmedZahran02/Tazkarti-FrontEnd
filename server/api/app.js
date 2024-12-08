2055fc6363d6496a7c8b8c455835cf16b0ed8be7
Kareem Alaa Abdullah <kareemalaa555@gmail.com>
2024-12-08 07:49:44 +0200

    .

--- server/api/app.js
+++ server/api/app.js
@@ -31,18 +31,7 @@ const { Server } = require("socket.io");
 const app = express();
 const allowedOrigins = ["https://not-tazkarti.vercel.app", "*"];
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  next();
-});
+app.use(cors());
 
 const DB = require("./utils/db");
 
