950b1714bc194326ab4ec8e14bbc74a410ae2cc6
Kareem Alaa Abdullah <kareemalaa555@gmail.com>
2024-12-08 07:38:31 +0200

    .

--- server/api/app.js
+++ server/api/app.js
@@ -32,16 +32,15 @@ const app = express();
 const allowedOrigins = ["https://not-tazkarti.vercel.app", "*"];
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://not-tazkarti.vercel.app"
-  ); // Frontend URL
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE",
-    "PUT"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   next();
 });
 
