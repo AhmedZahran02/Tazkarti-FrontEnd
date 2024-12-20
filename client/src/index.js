f29625eb4275e0a50f3067697d98a1a932449ba2
Kareem Alaa Abdullah <kareemalaa555@gmail.com>
2024-12-20 14:28:54 +0200

    .

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
-import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
